e9bdd5b999e2e999701ad4a3d646827242118d6b
ccar <ccar@ccardeMacBook-Pro.local>
2021-04-02 19:04:21 +0100

    创建views目录

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+
 import {
   HashRouter as Router,
   Switch,
   Route,
-  Link,
   Redirect
 } from "react-router-dom";
-import styled from 'styled-components';
-import Nav from './components/Nav';
-import Layout from './components/Layout';
+
+import Statistics from './views/Statistics';
+import Tags from './views/Tags';
+import Money from './views/Money';
+import NoMatch from './views/NoMatch';
 
 function App() {
   return (
@@ -30,34 +32,6 @@ function App() {
       </Switch>
     </Router>
   );
-}
-
-function NoMatch() {
-  return <h2>页面不存在，地址错误</h2>
-}
-
-function Tags() {
-  return (
-    <Layout>
-      <h2>标签页面</h2>
-    </Layout>
-  )
-}
-
-function Money() {
-  return (
-    <Layout>
-      <h2>记账页面</h2>
-    </Layout>
-  )
-}
-
-function Statistics() {
-  return (
-    <Layout>
-      <h2>统计页面</h2>
-    </Layout>
-  )
 }
 
 export default App;
