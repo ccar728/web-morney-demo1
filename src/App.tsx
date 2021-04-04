c9554ee8416c53990403ea5b05d2f37d5f4f0d24
ccar <ccar@ccardeMacBook-Pro.local>
2021-04-04 11:25:17 +0100

    完成money.tsx的TagsSection的UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import {
   HashRouter as Router,
@@ -12,25 +13,31 @@ import Tags from './views/Tags';
 import Money from './views/Money';
 import NoMatch from './views/NoMatch';
 
+const AppWrapper = styled.div`
+  color: #333;
+`
+
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route path="/tags">
-          <Tags />
-        </Route>
-        <Route path="/money">
-          <Money />
-        </Route>
-        <Route path="/statistics">
-          <Statistics />
-        </Route>
-        <Redirect exact from="/" to="/money" />
-        <Route path="*">
-          <NoMatch />
-        </Route>
-      </Switch>
-    </Router>
+    <AppWrapper>
+      <Router>
+        <Switch>
+          <Route path="/tags">
+            <Tags />
+          </Route>
+          <Route path="/money">
+            <Money />
+          </Route>
+          <Route path="/statistics">
+            <Statistics />
+          </Route>
+          <Redirect exact from="/" to="/money" />
+          <Route path="*">
+            <NoMatch />
+          </Route>
+        </Switch>
+      </Router>
+    </AppWrapper>
   );
 }
 
