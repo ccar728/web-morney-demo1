e65e49615cedb53194f264642998e06a70300973
ccar <ccar@ccardeMacBook-Pro.local>
2021-04-05 15:02:38 +0100

    完成tagSection的新增和选中功能

--- src/views/Money/TagsSection.tsx
+++ src/views/Money/TagsSection.tsx
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
+import React, {useState} from 'react';
 
-const TagsSection = styled.section`
+const Wrapper = styled.section`
   background: #fff;
   padding: 12px 16px;
   flex-grow: 1;
@@ -17,6 +18,9 @@ const TagsSection = styled.section`
       padding: 3px 16px;
       font-size: 14px;
       margin: 8px 12px;
+      &.selected{
+        background: #666;
+      }
     }
   }
   > button{
@@ -28,5 +32,44 @@ const TagsSection = styled.section`
     margin-top: 8px;
   }
 `
+
+const TagsSection: React.FC = (props)=>{
+  const [tags, setTags] = useState<string[]>(['衣','食','住','行'])
+  const [selectedTags, setSelectedTags] = useState<string[]>([])
+
+  const onAddTag = ()=>{
+    const tagName = window.prompt('新标签的名称为：')
+    if(tagName !== null){
+      setTags([...tags,tagName])
+    }
+  }
+
+  const onToggledTag = (tag:string)=>{
+    const index = selectedTags.indexOf(tag)
+    if(index >= 0){
+      setSelectedTags(selectedTags.filter(t => t !== tag))
+    }else{
+      setSelectedTags([...selectedTags, tag])
+    }
+  }
+
+  const getClass = (tag:string)=> selectedTags.indexOf(tag) >= 0 ? 'selected':''
+
+  return (
+    <Wrapper>
+      <ol>
+        {
+          tags.map(tag =>
+            <li key={tag} onClick={
+              ()=>{onToggledTag(tag)}
+            } className={getClass(tag)}>
+              {tag}</li>
+          )
+        }
+      </ol>
+      <button onClick={onAddTag}>新增标签</button>
+    </Wrapper>
+  )
+}
 
 export default TagsSection
