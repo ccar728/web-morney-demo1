3f89b59726740ee08696732a599f49c4c701b198
ccar <ccar@ccardeMacBook-Pro.local>
2021-04-07 12:52:37 +0100

    重构numpadsection.tsx

--- src/views/Money/NumPadSection.tsx
+++ src/views/Money/NumPadSection.tsx
@@ -1,59 +1,7 @@
 import styled from 'styled-components';
 import React, {useState} from 'react';
-
-const Wrapper = styled.section`
-  display: flex;
-  flex-direction: column;
-  > .output{
-    background: white;
-    font-size: 36px;
-    line-height: 72px;
-    text-align: right;
-    padding: 0 16px;
-    box-shadow: inset 0 -5px 5px -5px rgba(0,0,0,0.25),
-                inset 0 5px 5px -5px rgba(0,0,0,0.25);
-  }
-  > .pad{
-    > button{
-      float: left;
-      width: 25%;
-      height: 64px;
-      font-size: 18px;
-      border: 1px solid rgba(0,0,0,0.1);
-      &.ok{
-        height: 128px;
-        float: right;
-      }
-      &.zero{
-        width: 50%;
-      }
-      &:nth-child(1){
-        background-color: #f5f5f5;
-      }
-      &:nth-child(2),&:nth-child(5){
-        background-color: #d9d9d9;
-      }
-      &:nth-child(3),&:nth-child(6),&:nth-child(9){
-        background-color: #f5f5f5;
-      }
-      &:nth-child(4),&:nth-child(7),&:nth-child(10){
-        background-color: #d9d9d9;
-      }
-      &:nth-child(8),&:nth-child(11){
-        background-color: #f5f5f5;
-      }
-      &:nth-child(13){
-        background-color: #d9d9d9;
-      }
-      &:nth-child(12){
-        background-color: #d9d9d9;
-      }
-      &:nth-child(14){
-        background-color: #f5f5f5;
-      }
-    }
-  }
-`
+import Wrapper from './NumPadSection/Wrapper';
+import generateOutput from './NumPadSection/generateOutput';
 
 const NumPadSection:React.FC=()=>{
   const [output, _setOutput] = useState('0')
@@ -68,43 +16,11 @@ const NumPadSection:React.FC=()=>{
   const onClickButtonWrapper = (e:React.MouseEvent)=>{
     const text = (e.target as HTMLButtonElement).textContent
     if(text===null){return}
-    switch (text){
-      case '0':
-      case '1':
-      case '2':
-      case '3':
-      case '4':
-      case '5':
-      case '6':
-      case '7':
-      case '8':
-      case '9':
-        if(output === '0'){
-          setOutput(text)
-        }else{
-          setOutput(output+text)
-        }
-        break
-      case '.':
-        if(output.indexOf('.') >= 0){
-          return;
-        }
-        setOutput(output+'.')
-        break
-      case '删除':
-        if(output.length === 1){
-          setOutput('0')
-        }else{
-          setOutput(output.slice(0, -1))
-        }
-        break
-      case '清空':
-        setOutput('0')
-        break
-      case 'OK':
-        console.log('OK')
-        break
-
+    if(text === 'OK'){
+      return;
+    }
+    if('0123456789.'.split('').concat(['删除', '清空']).indexOf(text) >= 0){
+      setOutput(generateOutput(text, output))
     }
   }
   return(
